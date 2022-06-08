e6716b96f433bda4be622b38e6a19b24b3fca275
Bruno de Almeida <brunodealmeida17@yahoo.com>
2022-06-08 14:42:48 -0300

    model produtos e views, e testes Pytest

--- lojapy/settings.py
+++ lojapy/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     
     #my APPs
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
+    'products.apps.ProductsConfig',
+    
     
     
     
@@ -116,9 +119,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
